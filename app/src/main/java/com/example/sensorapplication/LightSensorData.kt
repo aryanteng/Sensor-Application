0855180402f694283b394b6bf36d1c85a81d50c7
aryanteng <aryanteng892@gmail.com>
2023-04-03 22:38:29 +0530

    added dependencies and initialised light sensor data

---
+++ app/src/main/java/com/example/sensorapplication/LightSensorData.kt
@@ -0,0 +1,9 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "light_sensor_data")
+data class LightSensorData(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val timestamp: Long,
+    val value: Float
+)
