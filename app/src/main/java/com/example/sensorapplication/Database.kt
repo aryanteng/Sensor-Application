8243ed0f6c5990d24fd37e40e15b8810fbedd12e
aryanteng <aryanteng892@gmail.com>
2023-04-03 22:45:44 +0530

    added Database.kt

---
+++ app/src/main/java/com/example/sensorapplication/Database.kt
@@ -0,0 +1,9 @@
+//import androidx.room.Database
+//import androidx.room.RoomDatabase
+//
+//@Database(entities = [ProximitySensorData::class, LightSensorData::class, GeomagneticRotationVectorSensorData::class], version = 1)
+//abstract class Database : RoomDatabase() {
+//    abstract fun proximitySensorDataDao(): ProximitySensorDataDao
+//    abstract fun lightSensorDataDao(): LightSensorDataDao
+//    abstract fun geomagneticRotationVectorSensorDataDao(): GeomagneticRotationVectorSensorDataDao
+//}
