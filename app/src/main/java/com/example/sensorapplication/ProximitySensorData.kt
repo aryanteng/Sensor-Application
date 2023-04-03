156c6a50b85ec0d9510a8a13bc73e22003a2e12e
aryanteng <aryanteng892@gmail.com>
2023-04-03 22:46:29 +0530

    proximity sensor data

---
+++ app/src/main/java/com/example/sensorapplication/ProximitySensorData.kt
@@ -0,0 +1,9 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "proximity_sensor_data")
+data class ProximitySensorData(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val timestamp: Long,
+    val value: Float
+)
