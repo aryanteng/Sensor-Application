8760af9dd16480d311336bcde8df8095dee96c2f
aryanteng <aryanteng892@gmail.com>
2023-04-03 23:18:30 +0530

    main activity

--- app/src/main/java/com/example/sensorapplication/Database.kt
+++ app/src/main/java/com/example/sensorapplication/Database.kt
@@ -1,9 +1,14 @@
-//import androidx.room.Database
-//import androidx.room.RoomDatabase
-//
-//@Database(entities = [ProximitySensorData::class, LightSensorData::class, GeomagneticRotationVectorSensorData::class], version = 1)
-//abstract class Database : RoomDatabase() {
-//    abstract fun proximitySensorDataDao(): com.example.sensorapplication.dao.ProximitySensorDataDao
-//    abstract fun lightSensorDataDao(): com.example.sensorapplication.dao.LightSensorDataDao
-//    abstract fun geomagneticRotationVectorSensorDataDao(): com.example.sensorapplication.dao.GeomagneticRotationVectorSensorDataDao
-//}
+package com.example.sensorapplication
+
+import GeomagneticRotationVectorSensorData
+import LightSensorData
+import ProximitySensorData
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [ProximitySensorData::class, LightSensorData::class, GeomagneticRotationVectorSensorData::class], version = 1)
+abstract class Database : RoomDatabase() {
+    abstract fun proximitySensorDataDao(): com.example.sensorapplication.dao.ProximitySensorDataDao
+    abstract fun lightSensorDataDao(): com.example.sensorapplication.dao.LightSensorDataDao
+    abstract fun geomagneticRotationVectorSensorDataDao(): com.example.sensorapplication.dao.GeomagneticRotationVectorSensorDataDao
+}
--- app/src/main/java/com/example/sensorapplication/MainActivity.kt
+++ app/src/main/java/com/example/sensorapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sensorapplication
 
+import GeomagneticRotationVectorSensorData
+import LightSensorData
 import ProximitySensorData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,13 +11,20 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.content.Context
 import androidx.room.Room
+import com.example.sensorapplication.dao.GeomagneticRotationVectorSensorDataDao
+import com.example.sensorapplication.dao.LightSensorDataDao
 import com.example.sensorapplication.dao.ProximitySensorDataDao
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private lateinit var proximitySensor: Sensor
+    private lateinit var lightSensor: Sensor
+    private lateinit var geomagneticSensor: Sensor
     private lateinit var proximitySensorDataDao: ProximitySensorDataDao
+    private lateinit var lightSensorDataDao: LightSensorDataDao
+    private lateinit var geomagneticSensorDataDao: GeomagneticRotationVectorSensorDataDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +36,21 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             Database::class.java, "sensor-data-db"
         ).build()
         proximitySensorDataDao = db.proximitySensorDataDao()
+        lightSensorDataDao = db.lightSensorDataDao()
+        geomagneticSensorDataDao = db.geomagneticRotationVectorSensorDataDao()
 
-        // Initialize proximity sensor
+        // Initialize sensors
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        geomagneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
     }
 
     override fun onResume() {
         super.onResume()
         sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, geomagneticSensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
@@ -44,15 +59,32 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
-            val value = event.values[0]
-            val timestamp = System.currentTimeMillis()
+        val value = event.values[0]
+        val timestamp = System.currentTimeMillis()
 
-            // Check if proximity sensor triggered by placing phone near ear or covering phone with hand
-            if (value < proximitySensor.maximumRange) {
-                // Store proximity sensor data in Room database
-                val data = ProximitySensorData(timestamp = timestamp, value = value)
-                proximitySensorDataDao.insert(data)
+        when (event.sensor.type) {
+            Sensor.TYPE_PROXIMITY -> {
+                // Check if proximity sensor triggered by placing phone near ear or covering phone with hand
+                if (value < proximitySensor.maximumRange) {
+                    // Store proximity sensor data in Room database
+                    val data = ProximitySensorData(timestamp = timestamp, value = value)
+                    proximitySensorDataDao.insert(data)
+                }
+            }
+            Sensor.TYPE_LIGHT -> {
+                // Store light sensor data in Room database
+                val data = LightSensorData(timestamp = timestamp, value = value)
+                lightSensorDataDao.insert(data)
+            }
+            Sensor.TYPE_MAGNETIC_FIELD -> {
+                val x = event.values[0]
+                val y = event.values[1]
+                val z = event.values[2]
+                val cos = sqrt(x * x + y * y + z * z.toDouble()).toFloat()
+
+                // Store geomagnetic sensor data in Room database
+                val data = GeomagneticRotationVectorSensorData(timestamp = timestamp, x = x, y = y, z = z, cos = cos)
+                geomagneticSensorDataDao.insert(data)
             }
         }
     }
