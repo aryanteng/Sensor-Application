78c38a2c221d11c3a84745c58a5547b759383300
aryanteng <aryanteng892@gmail.com>
2023-04-03 22:48:31 +0530

    geomagnetic rotation vector sensor

---
+++ app/src/main/java/com/example/sensorapplication/GeomagneticRotationVectorSensorData.kt
@@ -0,0 +1,12 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "geomagnetic_rotation_vector_sensor_data")
+data class GeomagneticRotationVectorSensorData(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val timestamp: Long,
+    val x: Float,
+    val y: Float,
+    val z: Float,
+    val cos: Float
+)
